98bbc0f38b10a5fc4109d7dc2a4379d71c5c312c
Joshua Oyedemi <josh.oyed@gmail.com>
2024-11-21 18:23:25 -0700

    src updated with driver and tests-- .py files

---
+++ src/driver.py
@@ -0,0 +1,9 @@
+"""
+this file will contain your code that generates analysis 
+results and generates any plots. This file should have one or more function 
+definitions that perform analysis tasks and should be called within the
+ if __name__ == "__main__":
+ guard at the bottom of the file
+"""
+
+import numpy as np 
--- src/linalg_interp.py
+++ src/linalg_interp.py
@@ -12,16 +12,12 @@ def gauss_iter_solve():
 
     """
         Parameters:
-        !!!!! A :
+
         Let 'A' be an array_like containing the coefficient matrix. 
-        !!!!!! b :
         Let 'b'in an array_like containing the right-hand-side vector(S). 
-        !!!!!! Xo
         Let 'Xo' ba an (optional) array_like containing the initial guess(es).
-        !!!!!! tol :
         We may define tol as an (optional) float that gives us the relative error tolerance 
         which is also the (stopping criterion) for our algorithm.
-        !!!!!! alg :
         Defining alg as an (optional) str flag for the alhorithm to be used .
 
         Note:
@@ -33,18 +29,15 @@ def gauss_iter_solve():
     """
 
 
-
+from scipy.interpolate import UnivariateSpline 
 
 def spline_function():
 
     """
         Parameters:
-        !!!!! xd:
-        Let us define xd as an array_like of float data increasing in value. 
-        !!!!! yd:
-        Let us define yd as an array_like of float data increasing in value 
+        Let us define 'xd' as an array_like of float data increasing in value. 
+        Let us define 'yd' as an array_like of float data increasing in value 
         or with same shape as xd.
-        !!!!! order:
         Let us define xd as an (optional) int  with possible values 
         1, 2, or 3 (default=3), and the return value is a function that takes one parameter (a 
         float or array_like of float) and returns the interpolated y value(s).
