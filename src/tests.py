ca2c71136fd12c95615fb8854b848995974b0ac3
Joshua Oyedemi <josh.oyed@gmail.com>
2024-11-29 20:32:50 -0700

    tests.py updated for errors

--- src/tests.py
+++ src/tests.py
@@ -1,4 +1,4 @@
-#Test Script
+# Test Script
 
 import os
 import numpy as np
@@ -7,7 +7,7 @@ from src.linalg_interp import gauss_iter_solve, cubic_spline
 
 def test_gauss_iter_solve():
 
-    #System description:
+    # System description:
     A = np.array([[4, -1, 0], [-1, 4, -1], [0, -1, 4]])
     b = np.array([15, 10, 10])
 
@@ -18,22 +18,38 @@ def test_gauss_iter_solve():
     np.testing.assert_almost_equal(x_seidel, x_expected, decimals=6)
     print("Gauss-seidel test passed!")
 
-#Test function to test the spline_function under the cubic spline function.
+# Test function to test the spline_function under the cubic spline function.
 def test_spline_function():
 
-    #Defining the test data
+    # Defining the test data.
     xd = np.array([0, 1, 2, 3, 4])
-    yd = np.array([0, 1, 2, 3, 4])
+    yd = xd**2
 
-    spline = cubic_spline(xd, yd, order=2)
+    # Invoking our cubic spline.
+    spline = cubic_spline(xd, yd, order=3)
 
+    # Trying for interpolation.
     x_test = np.array([0.5, 1.5, 2.5, 3.5])
     y_test = spline(x_test)
 
+    # Expected results.
     y_expected = x_test**2
 
+    # Results verification to check for equality.
     np.testing.assert_almost_equal(y_test, y_expected, decimal=6)
-    print("Cubic spline test passed!")
+    print("Cubic spline interpolating test passed!")
+
+
+    try:
+        spline(-1)
+    except ValueError as e:
+        print("Correctl raised error for x < xd[0]:", e)
+
+    try:
+        spline(5)
+    except ValueError as e:
+        print("Correct raised error for x > xd[-1]:", e)
+
 
 if __name__ == "__main__":
 
